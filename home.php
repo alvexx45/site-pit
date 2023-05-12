063e3c0a0784a628b269e3c6568167c8cf123460
bernardo <91989612+alvexx45@users.noreply.github.com>
2023-05-12 12:41:05 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Party Pros</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+    <h1>Área do Prestador de Serviço</h1>
+    <h2>Party Pros</h2>
+    <div class="box">
+        <a href="login.php">Login</a>
+        <a href="cadastro.php">Cadastre-se</a>
+    </div>
+</body>
+</html>
---
+++ salvar.php
@@ -0,0 +1,14 @@
+<?php
+    $dbhost = 'containers-us-west-69.railway.app';
+    $dbusername = 'root';
+    $dbpassword = 'GtsrGIjO6To6AX6Xvavm';
+    $dbname = 'id20721649_bancocadastro';
+
+    $conexao = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+    
+    // if($conexao->connect_errno){
+    //     echo "Erro";
+    // } else {
+    //     echo "Conectado";
+    // }    
+?>
